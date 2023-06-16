a3324b560fc44b474788d093283d2778f0a36e31
HermitSheep <94017494+HermitSheep@users.noreply.github.com>
2023-06-16 19:16:01 +0100

    scripts update

--- E3/web/script.js
+++ E3/web/script.js
@@ -14,131 +14,44 @@ function openSecondaryMenu(menuName) {
 function getSecondaryMenuContent(menuName) {
   switch (menuName) {
     case 'register_product':
-      return `
-        <div class="secondary-menu">
-          <h2>Register Products</h2>
-          <form action="register_product.cgi" method="post">
-            <label for="product_sku">Product SKU:</label>
-            <input type="text" name="reg_product_sku" id="product_sku" required>
-            <br><br>
-            <label for="product_name">Product Name:</label>
-            <input type="text" name="get_product_name" id="product_name" required>
-            <br><br>
-            <label for="product_description">Product Description:</label>
-            <textarea name="get_product_description" id="product_description" required></textarea>
-            <br><br>
-            <label for="product_price">Product Price:</label>
-            <input type="text" name="get_product_price" id="product_price" required>
-            <br><br>
-            <label for="product_ean">Product EAN:</label>
-            <input type="text" name="get_product_ean" id="product_ean">
-            <br><br>
-            <input type="submit" value="Submit">
-          </form>
-        </div>
-      `;
+      window.location.replace("register_product.html");
+      return;
 
     case 'remove_product':
       window.location.replace("remove_product.html");
       return;
 
     case 'register_supplier':
-      return `
-        <div class="secondary-menu">
-          <h2>Register Supplier</h2>
-          <form action="register_supplier.cgi" method="post">
-            <label for="reg_supplier_tin">Supplier TIN:</label>
-            <input type="text" name="reg_supplier_tin" id="reg_supplier_tin" required>
-            <br><br>
-            <label for="reg_supplier_name">Supplier Name:</label>
-            <input type="text" name="reg_supplier_name" id="reg_supplier_name" required>
-            <br><br>
-            <label for="reg_supplier_adress">Supplier Address:</label>
-            <textarea name="reg_supplier_address" id="reg_supplier_address" required></textarea>
-            <br><br>
-            <label for="product_sku">Product SKU:</label>
-            <input type="text" name="reg_product_sku" id="product_sku" required>
-            <br><br>
-            <label for="order_date">Order Date:</label>
-            <input type="date" name="order_date" id="order_date" required>
-            <br><br>
-            <input type="submit" value="Submit">
-          </form>
-        </div>
-      `;
+      window.location.replace("register_supplier.html");
+      return;
 
-      case 'remove_supplier':
-            window.location.replace("remove_supplier.html");
-            return;
+    case 'remove_supplier':
+      window.location.replace("remove_supplier.html");
+      return;
 
-      case 'change_product':
-            window.location.replace("change_product.html");
-            return;
+    case 'change_product':
+      window.location.replace("change_product.html");
+      return;
+
+    case 'register_client':
+      window.location.replace("register_client.html");
+      return;
+
+    case 'remove_client':
+      window.location.replace("remove_client.html");
+      return;
+
+    case 'make_order':
+      window.location.replace("make_order.html");
+      return;
+
+    case 'pay_order':
+      window.location.replace("pay_order.html");
+      return;
 
 
-      case 'register_client':
-            return `
-              <div class="secondary-menu">
-                <h2>Register Client</h2>
-                <form action="register_client.cgi" method="post">
-                <label for="customer_id">Customer ID:</label>
-                <input type="text" name="customer_id" id="customer_id" required>
-                <br><br>
-                <label for="name">Name:</label>
-                <input type="text" name="reg_name" id="name" required>
-                <br><br>
-                <label for="email">Email:</label>
-                <input type="email" name="reg_email" id="email" required>
-                <br><br>
-                <label for="phone">Phone:</label>
-                <input type="text" name="reg_phone" id="phone" required>
-                <br><br>
-                <label for="address">Address:</label>
-                <textarea name="reg_address" id="address" required></textarea>
-                <br><br>
-                <input type="submit" value="Submit">
-                </form>
-              </div>
-            `;
-
-
-      case 'remove_client':
-            window.location.replace("remove_client.html");
-            return;
-  
-      case 'make_order':
-            return `
-              <div class="secondary-menu">
-                <h2>Make Order</h2>
-                <form action="make_order.cgi" method="post">
-                <label for="order_id">Order ID:</label>
-                <input type="text" name="order_id" id="order_id" required>
-                <br><br>
-                <label for="customer_id">Customer ID:</label>
-                <input type="text" name="customer_id" id="customer_id" required>
-                <br><br>
-                <label for="product_sku">Product SKU:</label>
-                <input type="text" name="product_sku" id="product_sku" required>
-                <br><br>
-                <label for="order_date">Order Date:</label>
-                <input type="date" name="order_date" id="order_date" required>
-                <br><br>
-                <label for="quantity">Quantity:</label>
-                <input type="number" name="quantity" id="quantity" required>
-                <br><br>
-                <input type="submit" value="Submit">
-                </form>
-              </div>
-            `;
-
-      case 'pay_order':
-            window.location.replace("pay_order.html");
-            return;
-  
-      // Add other menu options with their respective HTML content
-  
-      default:
-        return '';
+    default:
+      return '';
     }
   }
   
